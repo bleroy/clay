a5ca6531f5d29f277c52ad45b8267d3b4418d3d7
Louis DeJardin <louis.dejardin@gmail.com>
2010-08-14 21:02:42 -0700

    Relocating to src subfolder

---
+++ src/ClaySharp.Tests/Behaviors/ClayFactoryBehaviorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using ClaySharp.Behaviors;
+using NUnit.Framework;
+
+namespace ClaySharp.Tests.Behaviors {
+    [TestFixture]
+    public class ClayFactoryBehaviorTests {
+        [Test]
+        public void InvokingMethodsCreateDynamicObjectWithBehaviors() {
+            dynamic factory = new Clay(new ClayFactoryBehavior());
+            object alpha = factory.Alpha();
+
+            Assert.That(alpha, Is.AssignableTo<IDynamicMetaObjectProvider>());
+            Assert.That(alpha, Is.AssignableTo<IClayBehaviorProvider>());
+
+        }
+    }
+}
